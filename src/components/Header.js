6d5818098f2795efc48ef1ab34c651ea9d727aa1
Ronald Santos <gabriellabuenodss@gmail.com>
2024-09-01 21:01:07 -0300

    cabecalho iniciado

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import "../App.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <p>home</p>
+            <p>|</p>
+            <p>contact</p>
+
+        </div>
+    )
+}
+
+export default Header
