d780525ac5346b66cf0670683402a2a20b43549c
Ronald Santos <gabriellabuenodss@gmail.com>
2024-09-06 12:46:32 -0300

    efeito menu adicionado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,38 @@
-import "../App.css"
-import React, {useState} from "react"
-import { useNavigate } from "react-router-dom"
+import "../App.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const navigation = useNavigate()
+    const [selected, setSelected] = useState(""); // Estado para item selecionado
+    const navigation = useNavigate();
 
-const navigateContactPage = () => {
-    navigation("textPage")    
-}    
+    const navigateContactPage = () => {
+        setSelected("contact"); // Define 'contact' como selecionado
+        navigation("textPage");
+    };
 
-const navigateHomePage = () => {
-    navigation("/")
-}
+    const navigateHomePage = () => {
+        setSelected("home"); // Define 'home' como selecionado
+        navigation("/");
+    };
 
     return (
         <div className="header">
-            <p className="menuItem" onClick={navigateHomePage}>home</p>
+            <p 
+                className={`menuItem ${selected === "home" ? "selected" : ""}`} 
+                onClick={navigateHomePage}
+            >
+                home
+            </p>
             <p>|</p>
-            <p className="menuItem" onClick={navigateContactPage}>contact</p>
-
+            <p 
+                className={`menuItem ${selected === "contact" ? "selected" : ""}`} 
+                onClick={navigateContactPage}
+            >
+                contact
+            </p>
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
